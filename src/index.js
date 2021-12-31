77cf7b58a0258d46543d9da416e2d31d26d295b6
starrything@gmail.com <giskfmfqhrh14!>
2021-12-31 14:23:07 +0900

    update path

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom";
 import "~/styles/index.css";
 import "~/styles/carousel.css";
 import App from "~/App";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "~/modules";
@@ -48,4 +48,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
